2da8b6ff4b901a3e14ef2b312587f86d2a68ca9c
Imogen Thompson <imogenrose00@gmail.com>
2022-06-11 14:48:34 -0400

    static dir

--- webApp/views.py
+++ webApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'webApp/post_list.html', {})
+    return render(request, 'webApp/login.html', {})
